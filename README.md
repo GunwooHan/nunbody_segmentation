db6ca14090de04e2ec91f06b40c58ffc215c7fd1
GunwooHan <gunwoohan@naver.com>
2021-11-09 13:52:12 +0900

    ADD pytorch lightning code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # nunbody_segmentation
+
+conda create -n nunbody python=3.8.8 -y
+conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.1 -c pytorch -y
+pip install matplotlib tqdm pycocotools opencv-python pytorch_lightning albumentations segmentation-models-pytorch wandb
+
+conda create -n nunbody python=3.8.8 -y && conda activate nunbody && pip install matplotlib tqdm pycocotools opencv-python pytorch_lightning albumentations segmentation-models-pytorch wandb
---
+++ server_27.sh
@@ -0,0 +1,29 @@
+#python train.py --gpus 2 --backbone timm-resnest101e
+#python train.py --gpus 2 --backbone timm-res2net50_26w_8s
+#python train.py --gpus 2 --backbone timm-regnetx_120
+#python train.py --gpus 2 --backbone se_resnet101
+#python train.py --gpus 2 --backbone inceptionv4
+#python train.py --gpus 2 --backbone timm-efficientnet-b6
+#python train.py --gpus 2 --backbone efficientnet-b6
+#python train.py --gpus 2 --backbone dpn92
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone timm-mobilenetv3_small_minimal_100
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone timm-mobilenetv3_small_100
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone timm-mobilenetv3_small_075
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone timm-mobilenetv3_large_minimal_100
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone timm-mobilenetv3_large_100
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone timm-mobilenetv3_large_075
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone timm-efficientnet-lite4
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone timm-efficientnet-lite3
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone timm-efficientnet-lite2
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone timm-efficientnet-lite1
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone timm-efficientnet-lite0
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone efficientnet-b0
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone efficientnet-b1
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone efficientnet-b2
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone efficientnet-b3
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone efficientnet-b4
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone efficientnet-b5
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone efficientnet-b6
+#python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone efficientnet-b7
+python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone efficientnet-b6
+python train.py --gpus 2 --ShiftScaleRotate 0.01 --ImageCompression 0.01 --HorizontalFlip 0.5 --VerticalFlip 0.5 --backbone efficientnet-b5
